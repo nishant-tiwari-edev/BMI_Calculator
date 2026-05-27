d103a4b03006caf4cb40eeab4d4f15cd27129fea
nishant-tiwari-edev <nishant06tiwari@gmail.com>
2026-05-27 22:32:42 +0530

    Added Input Validation and BMI categories

--- bmi_calculator.c
+++ bmi_calculator.c
@@ -19,13 +19,40 @@ int main()
     printf("Enter Your Height in CMs: ");
     scanf("%f", &ht);
 
-    printf("Your Height and Weight are %.2f cm and %.2f kg.\n", ht, wt);
+    // Input Validation
+    if(ht>0 && wt>0)
+    {
+        printf("Your Height and Weight are %.2f cm and %.2f kg.\n", ht, wt);
 
     // Calculating BMI with the function
-    bmi = BMI_CALC(wt, ht);
+        bmi = BMI_CALC(wt, ht);
 
     // Printing Result
-    printf("Your BMI is: %.2f\n", bmi);
+        printf("Your BMI is: %.2f\n", bmi);
+
+    // BMI Categories
+        if(bmi<=18.5){
+            printf("You are Underweight \n");
+        }
+
+        else if(bmi<=24.9){
+            printf("Congratulations! You have Normal Weight \n");
+        }
+
+        else if(bmi<=29.9){
+            printf("You are OverWeight \n");
+        }
+
+        else if(bmi>=30){
+            printf("You are Obese \n");
+        }
+
+    }
+
+    else
+    {
+        printf("Please Enter Valid Details");
+    }
 
     return 0;
 }
